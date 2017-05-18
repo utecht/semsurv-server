1b70ed004330b8c4042f6b6bf3e1eba4da4d1e90
Joseph Utecht <thenmal@gmail.com>
2017-05-18 11:02:07 -0500

    added baseline models

--- semsurv/semsurv/settings.py
+++ semsurv/semsurv/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'survey',
 ]
 
 MIDDLEWARE = [
@@ -85,18 +87,18 @@ DATABASES = {
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    # },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    # },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    # },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    # },
 ]
 
 
--- semsurv/survey/models.py
+++ semsurv/survey/models.py
@@ -1,3 +1,81 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Survey(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField(blank=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.title
+
+class Grouping(models.Model):
+    order = models.IntegerField()
+    name = models.CharField(max_length=50, blank=True)
+    survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
+    def __str__(self):
+        return "{}:{}-{}".format(self.survey.title[:10], self.order, self.name)
+
+class OptionGroup(models.Model):
+    name = models.CharField(max_length=50, blank=True)
+    randomize = models.BooleanField()
+    def __str__(self):
+        return self.name
+
+class Option(models.Model):
+    text = models.CharField(max_length=200)
+    str_value = models.CharField(max_length=200, blank=True)
+    int_value = models.IntegerField(blank=True)
+    float_value = models.FloatField(blank=True)
+    bool_value = models.BooleanField()
+    order = models.IntegerField(blank=True)
+    grouping = models.ForeignKey(OptionGroup, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.text
+
+QUESTION_TYPES = (('combo', 'Combo Box'),
+                  ('check', 'Check Boxes'),
+                  ('radio', 'Radio Buttons'),
+                  ('text', 'Text Field'),
+                  ('int', 'Integer Field'),
+                  ('scale', 'Scale'),
+                  ('nscale', 'Named Scale'),
+                  ('unit', 'Unit Int Field'),
+                  ('bool', 'Yes or No'))
+
+class Question(models.Model):
+    order = models.IntegerField()
+    grouping = models.ForeignKey(Grouping, on_delete=models.CASCADE)
+    text = models.TextField()
+    q_type = models.CharField(max_length=10, choices=QUESTION_TYPES)
+    options = models.ForeignKey(OptionGroup, blank=True, null=True,
+                                on_delete=models.CASCADE)
+    depends_string = models.CharField(max_length=200, blank=True)
+    def __str__(self):
+        return "{}.{}".format(self.order, self.text)
+
+class Respondent(models.Model):
+    email = models.EmailField(blank=True)
+    name = models.TextField(max_length=200, blank=True)
+
+class Response(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    respondent = models.ForeignKey(Respondent, on_delete=models.CASCADE)
+    str_value = models.CharField(max_length=200, blank=True)
+    int_value = models.IntegerField(blank=True)
+    float_value = models.FloatField(blank=True)
+    bool_value = models.BooleanField()
+    option = models.ForeignKey(Option, blank=True, null=True,
+                               on_delete=models.CASCADE)
+
+class Triple(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    subject = models.CharField(max_length=255)
+    predicate = models.CharField(max_length=255)
+    obj = models.CharField(max_length=255)
+    choice = models.ForeignKey(Option, on_delete=models.CASCADE,
+                               null=True, blank=True)
+    value = models.BooleanField()
+    def __str__(self):
+        return "{} - {} {} {}".format(self.question, self.subject,
+                                      self.predicate, self.obj)
